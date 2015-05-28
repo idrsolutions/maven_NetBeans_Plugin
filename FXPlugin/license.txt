8d94d3b716a5d102a711baaca32387edf31c47a7
hudson <hudson@hudsons-Mac-Pro.local>
2015-05-28 09:32:38 +0100

    Updated Souce

---
+++ FXPlugin/license.txt
@@ -0,0 +1,3 @@
+This code is copyright IDRsolutions. It may be freely distributed but without any warranty or support.
+
+IDRsolutions 2014
---
+++ FXPlugin/src/main/java/org/jpedal/netbeans/PDFViewerTypes.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jpedal.netbeans;
+
+/**
+ *
+ * @author markee
+ */
+public enum PDFViewerTypes {
+
+    BASE_VIEWERFX,INTERNAL_OPENVIWERFX,EXTERNAL_OPENVIEWERFX;
+
+}
