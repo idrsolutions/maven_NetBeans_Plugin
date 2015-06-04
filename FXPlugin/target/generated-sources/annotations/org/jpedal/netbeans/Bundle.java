dcffaf4fa4d7987a1ac0d94a8bba36251ae41533
hudson <hudson@hudsons-Mac-Pro.local>
2015-06-04 08:14:31 +0100

    Updated Souce

---
+++ FXPlugin/target/generated-sources/annotations/org/jpedal/netbeans/Bundle.java
@@ -0,0 +1,83 @@
+package org.jpedal.netbeans;
+/** Localizable strings for {@link org.jpedal.netbeans}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Oracle Guide</i>
+     * @see OracleGuideAction
+     */
+    static String CTL_OracleGuideAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_OracleGuideAction");
+    }
+    /**
+     * @return <i>PDFDisplay</i>
+     * @see PDFDisplayTopComponent
+     */
+    static String CTL_PDFDisplayAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PDFDisplayAction");
+    }
+    /**
+     * @return <i>PDFDisplay Window</i>
+     * @see PDFDisplayTopComponent
+     */
+    static String CTL_PDFDisplayTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PDFDisplayTopComponent");
+    }
+    /**
+     * @return <i>Open in Viewer</i>
+     * @see PDFOpenAction
+     */
+    static String CTL_PDFOpenAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PDFOpenAction");
+    }
+    /**
+     * @return <i>Base ViewerFX</i>
+     * @see PDFViewerAction
+     */
+    static String CTL_PDFViewerAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PDFViewerAction");
+    }
+    /**
+     * @return <i>ExternalFX</i>
+     * @see PDFViewerAction_ExternalFX
+     */
+    static String CTL_PDFViewerAction_ExternalFX() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PDFViewerAction_ExternalFX");
+    }
+    /**
+     * @return <i>OpenViewerFX</i>
+     * @see PDFViewerAction_OpenViewerFX
+     */
+    static String CTL_PDFViewerAction_OpenViewerFX() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PDFViewerAction_OpenViewerFX");
+    }
+    /**
+     * @return <i>This is a PDFDisplay window</i>
+     * @see PDFDisplayTopComponent
+     */
+    static String HINT_PDFDisplayTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_PDFDisplayTopComponent");
+    }
+    /**
+     * @return <i>Source</i>
+     * @see PDFDataObject
+     */
+    static String LBL_PDF_EDITOR() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_PDF_EDITOR");
+    }
+    /**
+     * @return <i>Files of PDF</i>
+     * @see PDFDataObject
+     */
+    static String LBL_PDF_LOADER() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_PDF_LOADER");
+    }
+    /**
+     * @return <i>Visual</i>
+     * @see PDFVisualElement
+     */
+    static String LBL_PDF_VISUAL() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_PDF_VISUAL");
+    }
+    private void Bundle() {}
+}
