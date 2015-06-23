a1217eca4c201d96ce8c741a521b1f00c8d67e6b
IDR Solutions <markstephens@idrsolutions.com>
2015-06-23 14:22:07 +0100

    Delete package-info.java

--- FXPlugin/src/main/java/org/jpedal/netbeans/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-@TemplateRegistration(folder = "Other", content = "PDFTemplate.PDF")
-package org.jpedal.netbeans;
-
-import org.netbeans.api.templates.TemplateRegistration;
